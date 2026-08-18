909166a6b41d41c99c3d644246773a6ff2b09bcd
tonbiattack <4.0497724e+07+tonbiattack@users.noreply.github.com>
2026-08-18 04:34:48 +0000

    外部API障害をRxJSで握り潰さず502へ伝播

--- src/items.service.ts
+++ src/items.service.ts
@@ -1,18 +1,25 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
-import { catchError, map, Observable, of } from 'rxjs';
+import { BadGatewayException, Injectable, Logger } from '@nestjs/common';
+import { catchError, map, Observable, throwError } from 'rxjs';
+import { AxiosError } from 'axios';
 
 export type Item = { id: number; name: string };
 
 @Injectable()
 export class ItemsService {
+  private readonly logger = new Logger(ItemsService.name);
+
   constructor(private readonly httpService: HttpService) {}
 
   getItems(): Observable<Item[]> {
     return this.httpService.get<{ items: Item[] }>(this.externalApiUrl()).pipe(
       map((response) => response.data.items),
-      // バグ: 下流障害を「正常な0件」に変換してしまう。
-      catchError(() => of([])),
+      catchError((error: AxiosError) => {
+        this.logger.error('外部APIの障害をBFFの例外へ変換', {
+          downstreamStatus: error.response?.status,
+        });
+        return throwError(() => new BadGatewayException('外部APIからデータを取得できませんでした'));
+      }),
     );
   }
 
